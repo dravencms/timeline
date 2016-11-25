15e79b47a2d595d67955a99f7f36f3395dde81b4
Adam Schubert <adam.schubert@sg1-game.net>
2016-11-25 20:50:34 +0100

    Script testing

---
+++ src/Dravencms/Timeline/Script/PostInstall.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Dravencms\Timeline\Script;
+
+use Dravencms\Model\Admin\Repository\MenuRepository;
+use Dravencms\Packager\IPackage;
+use Dravencms\Packager\IScript;
+
+/**
+ * Copyright (C) 2016 Adam Schubert <adam.schubert@sg1-game.net>.
+ */
+class PostInstall implements IScript
+{
+    public function __construct(MenuRepository $menuRepository)
+    {
+        dump($menuRepository->getOneByName('Users')->getName());
+    }
+
+    public function run(IPackage $package)
+    {
+        // TODO: Implement run() method.
+    }
+}
