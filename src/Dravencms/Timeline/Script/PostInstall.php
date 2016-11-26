bae300864e0b96324966c53f4aeb397946d39f40
Adam Schubert <adam.schubert@sg1-game.net>
2016-11-26 16:24:58 +0100

    Install scripts

--- src/Dravencms/Timeline/Script/PostInstall.php
+++ src/Dravencms/Timeline/Script/PostInstall.php
@@ -6,6 +6,8 @@ use Dravencms\Model\Admin\Entities\Menu;
 use Dravencms\Model\Admin\Repository\MenuRepository;
 use Dravencms\Model\User\Entities\AclOperation;
 use Dravencms\Model\User\Entities\AclResource;
+use Dravencms\Model\User\Repository\AclOperationRepository;
+use Dravencms\Model\User\Repository\AclResourceRepository;
 use Dravencms\Packager\IPackage;
 use Dravencms\Packager\IScript;
 use Kdyby\Doctrine\EntityManager;
@@ -15,37 +17,64 @@ use Kdyby\Doctrine\EntityManager;
  */
 class PostInstall implements IScript
 {
+    /** @var MenuRepository */
     private $menuRepository;
+
+    /** @var EntityManager */
     private $entityManager;
 
-    public function __construct(MenuRepository $menuRepository, EntityManager $entityManager)
+    /** @var AclResourceRepository */
+    private $aclResourceRepository;
+
+    /** @var AclOperationRepository */
+    private $aclOperationRepository;
+
+    /**
+     * PostInstall constructor.
+     * @param MenuRepository $menuRepository
+     * @param EntityManager $entityManager
+     * @param AclResourceRepository $aclResourceRepository
+     * @param AclOperationRepository $aclOperationRepository
+     */
+    public function __construct(MenuRepository $menuRepository, EntityManager $entityManager, AclResourceRepository $aclResourceRepository, AclOperationRepository $aclOperationRepository)
     {
         $this->menuRepository = $menuRepository;
         $this->entityManager = $entityManager;
+        $this->aclResourceRepository = $aclResourceRepository;
+        $this->aclOperationRepository = $aclOperationRepository;
     }
 
+    /**
+     * @param IPackage $package
+     * @throws \Exception
+     */
     public function run(IPackage $package)
     {
-        $aclResource = new AclResource('timeline', 'Timeline');
-
-        $this->entityManager->persist($aclResource);
-
-        $aclOperationEdit = new AclOperation($aclResource, 'edit', 'Allows editation of timeline');
-        $this->entityManager->persist($aclOperationEdit);
-        $aclOperationDelete = new AclOperation($aclResource, 'delete', 'Allows deletion of timeline');
-        $this->entityManager->persist($aclOperationDelete);
-
-        $adminMenu = new Menu('Timeline', ':Admin:Timeline:Group', 'fa-bars', $aclOperationEdit);
-
-        $foundRoot = $this->menuRepository->getOneByName('Site items');
-
-        if ($foundRoot)
-        {
-            $this->menuRepository->getMenuRepository()->persistAsLastChildOf($adminMenu, $foundRoot);
+        if (!$aclResource = $this->aclResourceRepository->getOneByName('timeline')) {
+            $aclResource = new AclResource('timeline', 'Timeline');
+            $this->entityManager->persist($aclResource);
         }
-        else
-        {
-            $this->entityManager->persist($adminMenu);
+
+        if (!$aclOperationEdit = $this->aclOperationRepository->getOneByName('edit')) {
+            $aclOperationEdit = new AclOperation($aclResource, 'edit', 'Allows editation of timeline');
+            $this->entityManager->persist($aclOperationEdit);
+        }
+
+        if (!$aclOperationDelete = $this->aclOperationRepository->getOneByName('delete')) {
+            $aclOperationDelete = new AclOperation($aclResource, 'delete', 'Allows deletion of timeline');
+            $this->entityManager->persist($aclOperationDelete);
+        }
+
+        if (!$this->menuRepository->getOneByPresenter(':Admin:Timeline:Group')) {
+            $adminMenu = new Menu('Timeline', ':Admin:Timeline:Group', 'fa-bars', $aclOperationEdit);
+
+            $foundRoot = $this->menuRepository->getOneByName('Site items');
+
+            if ($foundRoot) {
+                $this->menuRepository->getMenuRepository()->persistAsLastChildOf($adminMenu, $foundRoot);
+            } else {
+                $this->entityManager->persist($adminMenu);
+            }
         }
 
         $this->entityManager->flush();
